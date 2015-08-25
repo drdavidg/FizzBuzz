635ccc14c744945abb38fc2ea99555e45ffba9ec
David Gonen <david@curiousminds.com>
2015-08-25 11:50:30 +0300

    added some commenting

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
   function numberPrompt (msg) {
     var x = prompt(msg);
     if (x === null) { //otherwise theres no way to get out of the prompt except putting in a valid number
@@ -8,17 +7,16 @@ $(document).ready(function(){
     }
     x = +x; //convert string from prompt to a number
 
-    if (!parseInt(x)) {
+    if (!parseInt(x)) {  //check if its a string
       return numberPrompt("You entered a string!!! Enter a number.");
     }
-    else if (x % 1 !== 0) return numberPrompt("Please enter a number with NO DECIMAL PLACES!");
+    else if (x % 1 !== 0) return numberPrompt("Please enter a number with NO DECIMAL PLACES!");  //check for decimal places
     else {
-      return Fizzy(x);
+      return Fizzy(x);  //x passed all the checks, so run Fizzy function on it to generate FizzBuzz
     }
   }
 
-
-  function Fizzy(n) {
+  function Fizzy(n) { //didn't refactor my original FizzBuzz code (i now know there are many ways to code this, some better than this)
     for (i =1; i <= n; i++) {
       if ( ((i % 3) === 0) && ((i % 5) === 0) ) {
         $('body').append('<div>fizz buzz</div>');
