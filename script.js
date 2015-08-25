1d3ddd33b6d16a63e943f44752851f77896cdb44
David Gonen <david@curiousminds.com>
2015-08-25 11:28:57 +0300

    refactoring fizzbuzz app

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 $(document).ready(function(){
+  var userNumber = prompt("Please enter a number");
+  userNumber = +userNumber;
 
-  for (i = 1; i <= 100; i++) {
-    if ( ((i % 3) == 0) && ((i % 5) == 0) ) {
-      $('body').append('<div>fizz buzz</div>');
+  ValidateInput(userNumber);
+
+  function ValidateInput (x) {
+    if (!(parseInt(x))) {
+      return $('body').append('<div>Please enter a number!</div>'); 
     }
 
-    else if ((i % 3) == 0) {
-      $('body').append('<div>fizz</div>');
-    }
-    else if ((i % 5) == 0) {
-      $('body').append('<div>buzz</div>');
-    }
-    else {
-      $('body').append("<div>" + i + "</div>");
-    }
   }
+
+  function Fizzy(n) {
+    for (i =1; i <= n; i++) {
+      if ( ((i % 3) == 0) && ((i % 5) == 0) ) {
+        $('body').append('<div>fizz buzz</div>');
+      }
+      else if ((i % 3) == 0) {
+        $('body').append('<div>fizz</div>');
+      }
+      else if ((i % 5) == 0) {
+        $('body').append('<div>buzz</div>');
+      }
+      else {
+        $('body').append("<div>" + i + "</div>");
+      }
+    }
+
+  };
+
+  Fizzy(userNumber);
 
 });
